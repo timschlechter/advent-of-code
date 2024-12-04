9f7ce42e9569dd0abc62829ba109de5787977306
Tim Schlechter <timschlechter@gmail.com>
2024-12-04 16:53:27 +0100

    Move patterns array outside mapping

--- 2024/day4/index.ts
+++ 2024/day4/index.ts
@@ -5,24 +5,22 @@ const parse = (input: string): Grid<string> =>
 
 export const part1 = (input: string) => {
   const grid = parse(input);
-
   return grid.findPattern(['X', 'M', 'A', 'S']).length;
 };
 
 export const part2 = (input: string) => {
   const grid = parse(input);
-
+  const patterns = ['MSMS', 'SMSM', 'MMSS', 'SSMM'];
+  
   return grid.cells
     .filter((cell) => cell.value === 'A')
-    .flatMap(
-      (cell) =>
-        ['MSMS', 'SMSM', 'MMSS', 'SSMM'].filter(
-          (pattern) =>
-            cell.topLeft?.value === pattern[0] &&
-            cell.topRight?.value === pattern[1] &&
-            cell.bottomLeft?.value === pattern[2] &&
-            cell.bottomRight?.value === pattern[3],
-        ),
-    )
-    .length;
+    .flatMap((cell) =>
+      patterns.filter(
+        (pattern) =>
+          cell.topLeft?.value === pattern[0] &&
+          cell.topRight?.value === pattern[1] &&
+          cell.bottomLeft?.value === pattern[2] &&
+          cell.bottomRight?.value === pattern[3],
+      ),
+    ).length;
 };

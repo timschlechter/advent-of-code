7257e2e0d429924e86b2f00d57de1a1ab887d6c9
Tim Schlechter <timschlechter@gmail.com>
2024-12-04 12:59:44 +0100

    Refactor readExample

--- 2024/day3/index.spec.ts
+++ 2024/day3/index.spec.ts
@@ -1,4 +1,4 @@
-import { readExample, readInput, readExample2 } from '../utils';
+import { readExample, readInput } from '../utils';
 import { part1, part2 } from '.';
 
 describe('day2', () => {
@@ -11,7 +11,7 @@ describe('day2', () => {
     expect(part1(input)).toBe(161289189);
   });
   it('part2 example', async () => {
-    const example = await readExample2(__dirname);
+    const example = await readExample(__dirname, 2);
     expect(part2(example)).toBe(48);
   });
   it('part2 solution', async () => {
--- 2024/utils/index.ts
+++ 2024/utils/index.ts
@@ -6,10 +6,6 @@ export * from './Grid';
 export const readInput = (folder: string) =>
   fs.readFile(path.resolve(folder, 'input.txt'), 'utf-8');
 
-export const readExample = (folder: string) =>
-  fs.readFile(path.resolve(folder, 'example.txt'), 'utf-8');
-
-export const readExample2 = (folder: string) =>
-  fs.readFile(path.resolve(folder, 'example2.txt'), 'utf-8');
-
+export const readExample = (folder: string, number?: number) =>
+  fs.readFile(path.resolve(folder, `example${number??''}.txt`), 'utf-8');
 

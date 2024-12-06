a00d42eb9155a43690e243f1297d402cbec9cf48
Tim Schlechter <timschlechter@gmail.com>
2024-12-06 23:34:15 +0100

    Add day template

---
+++ 2024/day7/index.ts
@@ -0,0 +1,15 @@
+import { Direction, opposite, Grid, rotate90 } from '../utils';
+
+type ParsedInput = {};
+
+const parse = (input: string): ParsedInput => {
+  return {};
+};
+
+export const part1 = (input: string) => {
+  return parse(input);
+};
+
+export const part2 = (input: string) => {
+  return parse(input);
+};

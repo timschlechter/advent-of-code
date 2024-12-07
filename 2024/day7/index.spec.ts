79ee57caaaa5fc5cf27256228ea4cdaf9d0958d3
Tim Schlechter <timschlechter@gmail.com>
2024-12-07 08:37:46 +0100

    day 7

--- 2024/day7/index.spec.ts
+++ 2024/day7/index.spec.ts
@@ -4,18 +4,18 @@ import { part1, part2 } from '.';
 describe('day7', () => {
   it('part1 example', async () => {
     const example = await readExample(__dirname);
-    expect(part1(example)).toBe(0);
+    expect(part1(example)).toBe(3749);
   });
   it('part1 solution', async () => {
     const input = await readInput(__dirname);
-    expect(part1(input)).toBe(0);
+    expect(part1(input)).toBe(6392012777720);
   });
   it('part2 example', async () => {
     const example = await readExample(__dirname);
-    expect(part2(example)).toBe(0);
+    expect(part2(example)).toBe(11387);
   });
   it('part2 solution', async () => {
     const input = await readInput(__dirname);
-    expect(part2(input)).toBe(0);
+    expect(part2(input)).toBe(61561126043536);
   });
 });
--- 2024/day7/index.ts
+++ 2024/day7/index.ts
@@ -1,15 +1,51 @@
-import { Direction, opposite, Grid, rotate90 } from '../utils';
-
-type ParsedInput = {};
-
-const parse = (input: string): ParsedInput => {
-  return {};
+type Equation = {
+  target: number;
+  start: number;
+  values: number[];
 };
 
-export const part1 = (input: string) => {
-  return parse(input);
+const parse = (input: string): Equation[] =>
+  input
+    .split('\n')
+    .map((line) => line.split(' ').map((x) => parseInt(x)))
+    .map((line) => {
+      const [target, start, ...values] = line;
+      return { target, start, values };
+    });
+
+const calculate = (a: number, b: number, op: string): number => {
+  if (op === '+') return a + b;
+  if (op === '*') return a * b;
+  if (op === '||') return parseInt(`${a}${b}`, 10);
+
+  throw new Error(`Unsupported operator: ${op}`);
 };
 
-export const part2 = (input: string) => {
-  return parse(input);
+const canSolve = (eq: Equation, operators: string[]): boolean => {
+  const { target, start, values } = eq;
+
+  if (values.length === 0) {
+    return start === target;
+  }
+
+  return operators.some((op) =>
+    canSolve(
+      {
+        target,
+        start: calculate(start, values[0], op),
+        values: values.slice(1),
+      },
+      operators,
+    ),
+  );
 };
+
+export const part1 = (input: string) =>
+  parse(input)
+    .filter((eq) => canSolve(eq, ['+', '*']))
+    .reduce((acc, curr) => acc + curr.target, 0);
+
+export const part2 = (input: string) =>
+  parse(input)
+    .filter((eq) => canSolve(eq, ['+', '*', '||']))
+    .reduce((acc, curr) => acc + curr.target, 0);

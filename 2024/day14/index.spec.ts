a5aee40bcda8a874d8c986e1f830b1e2166f2a5b
Tim Schlechter <timschlechter@gmail.com>
2024-12-14 10:19:05 +0100

    Day 14 part 2

--- 2024/day14/index.spec.ts
+++ 2024/day14/index.spec.ts
@@ -1,21 +1,17 @@
 import { readExample, readInput } from '../utils';
 import { part1, part2 } from '.';
 
-describe('day8', () => {
+describe('day14', () => {
   it('part1 example', async () => {
     const example = await readExample(__dirname);
     expect(part1(example, 11, 7)).toBe(12);
   });
   it('part1 solution', async () => {
     const input = await readInput(__dirname);
-    expect(part1(input, 101, 103)).toBe(0);
+    expect(part1(input, 101, 103)).toBe(230435667);
   });
-  // it('part2 example', async () => {
-  //   const example = await readExample(__dirname);
-  //   expect(part2(example)).toBe(0);
-  // });
-  // it('part2 solution', async () => {
-  //   const input = await readInput(__dirname);
-  //   expect(part2(input)).toBe(0);
-  // });
+  it('part2 solution', async () => {
+    const input = await readInput(__dirname);
+    expect(part2(input)).toBe(7709);
+  });
 });
--- 2024/day14/index.ts
+++ 2024/day14/index.ts
@@ -1,59 +1,47 @@
-import { Cell } from '../utils';
+type Robot = { x: number; y: number; dX: number; dY: number };
 
-type Robot = { pX: number; pY: number; vX: number; vY: number };
-
-const parse = (input: string): Robot[] => {
-  return input.split('\n').map((line) => {
+const parse = (input: string): Robot[] =>
+  input.split('\n').map((line) => {
     const match = line.match(/p=(\d+),(\d+)\sv=(-?\d+),(-?\d+)/);
-    const [pX, pY, vX, vY] = match!.slice(1).map((x) => parseInt(x, 10));
-    return { pX, pY, vX, vY };
+    const [x, y, dX, dY] = match!.slice(1).map((x) => parseInt(x, 10));
+    return { x, y, dX, dY };
   });
-};
 
 const render = (robots: Robot[], w: number, h: number) => {
   let s = '';
   for (let i = 0; i < h; i++) {
     for (let j = 0; j < w; j++) {
-      s += robots.filter((r) => r.pX === j && r.pY === i).length || '.';
+      s += robots.filter((r) => r.x === j && r.y === i).length || '.';
     }
     s += '\n';
   }
   return s;
 };
 
-export const part1 = (input: string, W: number, H: number) => {
+export const part1 = (input: string, width: number, height: number) => {
   const robots = parse(input);
 
   for (let i = 0; i < 100; i++) {
     for (const robot of robots) {
-      robot.pX += robot.vX;
-      robot.pY += robot.vY;
-
-      if (robot.pX >= W) {
-        robot.pX = robot.pX % W;
-      } else if (robot.pX < 0) {
-        robot.pX = W - (Math.abs(robot.pX) % W);
-      }
-      if (robot.pY >= H) {
-        robot.pY = robot.pY % H;
-      } else if (robot.pY < 0) {
-        robot.pY = H - (Math.abs(robot.pY) % H);
-      }
+      robot.x = (robot.x + (robot.dX % width) + width) % width;
+      robot.y = (robot.y + (robot.dY % height) + height) % height;
     }
   }
 
   const quadrants = [0, 0, 0, 0];
+  const midX = (width - 1) / 2;
+  const midY = (height - 1) / 2;
   for (const robot of robots) {
-    if (robot.pX < (W - 1) / 2) {
-      if (robot.pY < (H - 1) / 2) {
+    if (robot.x < midX) {
+      if (robot.y < midY) {
         quadrants[0]++;
-      } else if (robot.pY > (H - 1) / 2) {
+      } else if (robot.y > midY) {
         quadrants[1]++;
       }
-    } else if (robot.pX > (W - 1) / 2) {
-      if (robot.pY < (H - 1) / 2) {
+    } else if (robot.x > midX) {
+      if (robot.y < midY) {
         quadrants[2]++;
-      } else if (robot.pY > (H - 1) / 2) {
+      } else if (robot.y > midY) {
         quadrants[3]++;
       }
     }
@@ -63,5 +51,24 @@ export const part1 = (input: string, W: number, H: number) => {
 };
 
 export const part2 = (input: string) => {
-  return parse(input);
+  const robots = parse(input);
+
+  const width = 101;
+  const height = 103;
+
+  for (let i = 0; i < Number.MAX_SAFE_INTEGER; i++) {
+    // Hope to see a xmas tree when all robots are on a unique position
+    const allOnUniquePos = !robots.find((a) =>
+      robots.find((b) => b != a && b.x === a.x && b.y === a.y),
+    );
+    if (allOnUniquePos) {
+      console.log(i, render(robots, width, height));
+      return i;
+    }
+
+    for (const robot of robots) {
+      robot.x = (robot.x + (robot.dX % width) + width) % width;
+      robot.y = (robot.y + (robot.dY % height) + height) % height;
+    }
+  }
 };
